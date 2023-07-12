b064d4e1a970865dd114b698e869d65c04e18926
Karlo Videk <karlo.videk@gmail.com>
2023-07-12 15:00:03 +0200

    ex periment

--- static/js/components/FurnitureConfigurator.js
+++ static/js/components/FurnitureConfigurator.js
@@ -44,14 +44,17 @@ export default class FurnitureConfigurator {
             };
 
             this.initModel();
+            this.addLights();
+            this.addEnvironmentLight();
+            this.addFloor();
             this.animate();
         }
     }
 
     initModel() {
         // camera
-        this.camera = new THREE.PerspectiveCamera(35, this.width / this.height, 0.5, 400);
-        this.camera.position.set(17, 10, 17);
+        this.camera = new THREE.PerspectiveCamera(40, this.width / this.height, 1, 40);
+        this.camera.position.set(0, 5, 15);
 
         // scene
         this.scene = new THREE.Scene();
@@ -80,7 +83,7 @@ export default class FurnitureConfigurator {
         this.renderer.setPixelRatio(window.devicePixelRatio);
         this.renderer.setSize(this.width, this.height);
         this.renderer.shadowMap.enabled = true;
-        this.renderer.gammaFactor = 2.2;
+        this.renderer.gammaFactor = 2;
         this.renderer.outputColorSpace = "srgb-linear";
         this.renderer.useLegacyLights = false;
         this.renderer.shadowMap.type = THREE.PCFShadowMap;
@@ -88,12 +91,12 @@ export default class FurnitureConfigurator {
         this.renderer.toneMappingExposure = 1;
         this.modelContainer.appendChild(this.renderer.domElement);
 
-        const environment = new RoomEnvironment();
-        this.pmremGenerator = new THREE.PMREMGenerator(this.renderer);
-        this.scene.environment = this.pmremGenerator.fromScene(environment).texture;
+        // const environment = new RoomEnvironment();
+        // this.pmremGenerator = new THREE.PMREMGenerator(this.renderer);
+        // this.scene.environment = this.pmremGenerator.fromScene(environment).texture;
 
         // loader
-        this.loadModel();
+        // this.loadModel();
         this.loadSphere();
 
         // orbit controls
@@ -117,185 +120,36 @@ export default class FurnitureConfigurator {
     }
 
     loadModel() {
+        const materialScale = 2.5;
+
         const materials = {
-            mat1: {
-                base: this.texture.load("../static/models/mat1/base.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-                height: this.texture.load("../static/models/mat1/height.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-                ao: this.texture.load("../static/models/mat1/ao.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-                norm: this.texture.load("../static/models/mat1/norm.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-                rough: this.texture.load("../static/models/mat1/rough.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-            },
             mat2: {
-                base: this.texture.load("../static/models/mat2/base.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-                height: this.texture.load("../static/models/mat2/height.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-                ao: this.texture.load("../static/models/mat2/ao.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-                norm: this.texture.load("../static/models/mat2/norm.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-                rough: this.texture.load("../static/models/mat2/rough.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
+                base: this.texture.load("../static/models/mat1/base.jpg"),
+                height: this.texture.load("../static/models/mat1/height.jpg"),
+                ao: this.texture.load("../static/models/mat1/ao.jpg"),
+                norm: this.texture.load("../static/models/mat1/norm.jpg"),
+                rough: this.texture.load("../static/models/mat1/rough.jpg"),
+            },
+            mat1: {
+                base: this.texture.load("../static/models/mat2/base.jpg"),
+                height: this.texture.load("../static/models/mat2/height.jpg"),
+                ao: this.texture.load("../static/models/mat2/ao.jpg"),
+                norm: this.texture.load("../static/models/mat2/norm.jpg"),
+                rough: this.texture.load("../static/models/mat2/rough.jpg"),
             },
             mat3: {
-                base: this.texture.load("../static/models/matLion/base.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-                height: this.texture.load("../static/models/matLion/height.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-                ao: this.texture.load("../static/models/matLion/ao.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-                norm: this.texture.load("../static/models/matLion/norm.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-                rough: this.texture.load("../static/models/matLion/rough.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
+                base: this.texture.load("../static/models/matLion/base.jpg"),
+                height: this.texture.load("../static/models/matLion/height.jpg"),
+                ao: this.texture.load("../static/models/matLion/ao.jpg"),
+                norm: this.texture.load("../static/models/matLion/norm.jpg"),
+                rough: this.texture.load("../static/models/matLion/rough.jpg"),
             },
         };
 
         let material = new THREE.MeshPhysicalMaterial({
             map: materials.mat1.base,
             aoMap: materials.mat1.ao,
-            aoMapIntensity: 1,
-            normalMap: materials.mat1.norm,
-            displacementMap: materials.mat1.height,
-            displacementScale: 0,
-            roughnessMap: materials.mat1.rough,
-            metalness: 0,
-            clearcoat: 0,
-            flatShading: false,
-        });
-
-        material.map.minFilter = THREE.NearestFilter;
-        material.map.generateMipmaps = false;
-        material.map.wrapS = material.map.wrapT = THREE.RepeatWrapping;
-        material.aoMap.wrapS = material.aoMap.wrapT = THREE.RepeatWrapping;
-        material.displacementMap.wrapS = material.displacementMap.wrapT = THREE.RepeatWrapping;
-        material.normalMap.wrapS = material.normalMap.wrapT = THREE.RepeatWrapping;
-        material.roughnessMap.wrapS = material.roughnessMap.wrapT = THREE.RepeatWrapping;
-
-        material.map.repeat.set(0.1, 0.1);
-        material.aoMap.repeat.set(0.1, 0.1);
-        material.displacementMap.repeat.set(0.1, 0.1);
-        material.normalMap.repeat.set(0.1, 0.1);
-        material.roughnessMap.repeat.set(0.1, 0.1);
-
-        // get model
-        let model = this.modelContainer.getAttribute("data-model-source");
-
-        // loader
-        const loader = new GLTFLoader();
-
-        const dracoLoader = new DRACOLoader();
-        dracoLoader.setDecoderPath("../static/js/vendors/draco/");
-        loader.setDRACOLoader(dracoLoader);
-
-        loader.load(model, (model) => {
-            model.scene.position.x = -4;
-            material.color.convertSRGBToLinear();
-
-            model.scene.traverse((object) => {
-                if (object.isMesh) {
-                    object.castShadow = true;
-                    object.receiveShadow = true;
-                    object.material.color.convertSRGBToLinear();
-
-                    // initial material setup
-                    object.material = material;
-                }
-            });
-
-            this.gui
-                .add(this.guiConf, "sofaMaterial", {
-                    Material1: 1,
-                    Material2: 2,
-                    Material3: 3,
-                })
-                .onChange((value) => {
-                    this.transformMaterial(value, material, materials, 0.1);
-                });
-
-            this.scene.add(model.scene);
-        });
-    }
-
-    loadSphere() {
-        const materials = {
-            mat1: {
-                base: this.texture.load("../static/models/mat1/base.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-                height: this.texture.load("../static/models/mat1/height.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-                ao: this.texture.load("../static/models/mat1/ao.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-                norm: this.texture.load("../static/models/mat1/norm.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-                rough: this.texture.load("../static/models/mat1/rough.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-            },
-            mat2: {
-                base: this.texture.load("../static/models/mat2/base.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-                height: this.texture.load("../static/models/mat2/height.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-                ao: this.texture.load("../static/models/mat2/ao.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-                norm: this.texture.load("../static/models/mat2/norm.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-                rough: this.texture.load("../static/models/mat2/rough.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-            },
-            mat3: {
-                base: this.texture.load("../static/models/matLion/base.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-                height: this.texture.load("../static/models/matLion/height.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-                ao: this.texture.load("../static/models/matLion/ao.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-                norm: this.texture.load("../static/models/matLion/norm.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-                rough: this.texture.load("../static/models/matLion/rough.jpg", (tex) => {
-                    tex.colorSpace = "srgb-linear";
-                }),
-            },
-        };
-
-        let material = new THREE.MeshPhysicalMaterial({
-            map: materials.mat1.base,
-            aoMap: materials.mat1.ao,
-            aoMapIntensity: 1,
+            aoMapIntensity: 0,
             normalMap: materials.mat1.norm,
             displacementMap: materials.mat1.height,
             displacementScale: 0.03,
@@ -313,11 +167,156 @@ export default class FurnitureConfigurator {
         material.normalMap.wrapS = material.normalMap.wrapT = THREE.RepeatWrapping;
         material.roughnessMap.wrapS = material.roughnessMap.wrapT = THREE.RepeatWrapping;
 
-        material.map.repeat.set(3, 3);
-        material.aoMap.repeat.set(3, 3);
-        material.displacementMap.repeat.set(3, 3);
-        material.normalMap.repeat.set(3, 3);
-        material.roughnessMap.repeat.set(3, 3);
+        material.map.repeat.set(materialScale, materialScale);
+        material.aoMap.repeat.set(materialScale, materialScale);
+        material.displacementMap.repeat.set(materialScale, materialScale);
+        material.normalMap.repeat.set(materialScale, materialScale);
+        material.roughnessMap.repeat.set(materialScale, materialScale);
+
+        // get model
+        let model = this.modelContainer.getAttribute("data-model-source");
+
+        // loader
+        const loader = new GLTFLoader();
+
+        const dracoLoader = new DRACOLoader();
+        dracoLoader.setDecoderPath("../static/js/vendors/draco/");
+        loader.setDRACOLoader(dracoLoader);
+
+        loader.load(model, (model) => {
+            // model.scene.scale.set(0.05, 0.05, 0.05);
+            model.scene.position.x = -4;
+            material.color.convertSRGBToLinear();
+
+            const objects = [model.scene.getObjectByName("chair_A"), model.scene.getObjectByName("chair_B"), model.scene.getObjectByName("chair_C")];
+
+            objects.forEach((object) => {
+                object.castShadow = true;
+                object.receiveShadow = true;
+                object.material.color.convertSRGBToLinear();
+
+                // initial material setup
+                object.material = material;
+            });
+
+            this.gui
+                .add(this.guiConf, "sofaMaterial", {
+                    Material1: 1,
+                    Material2: 2,
+                    Material3: 3,
+                })
+                .onChange((value) => {
+                    this.transformMaterial(value, material, materials, materialScale);
+                });
+
+            this.scene.add(model.scene);
+        });
+    }
+
+    addLights() {
+        const lightHelpers = false;
+
+        const directionalLight1 = new THREE.DirectionalLight(0xf0f0f0, 2);
+        directionalLight1.position.set(0, 6, 3);
+        const helper1 = new THREE.DirectionalLightHelper(directionalLight1, 2);
+
+        directionalLight1.castShadow = true;
+        this.scene.add(directionalLight1);
+
+        const directionalLight2 = new THREE.DirectionalLight(0xf0f0f0, 2);
+        directionalLight2.position.set(0, 3, 8);
+        const helper2 = new THREE.DirectionalLightHelper(directionalLight2, 2);
+
+        directionalLight2.castShadow = true;
+        this.scene.add(directionalLight2);
+
+        const pointLight = new THREE.PointLight(0xffffff, 3);
+        const helper3 = new THREE.PointLightHelper(pointLight, 2);
+        pointLight.position.set(0, 12, 0);
+
+        pointLight.castShadow = true;
+        this.scene.add(pointLight);
+
+        if (lightHelpers) {
+            this.scene.add(helper1);
+            this.scene.add(helper2);
+            this.scene.add(helper3);
+        }
+    }
+
+    addEnvironmentLight() {
+        const light = new THREE.AmbientLight(0x808080, 4);
+        this.scene.add(light);
+    }
+
+    addFloor() {
+        const geometry = new THREE.PlaneGeometry(40, 40);
+        const material = new THREE.MeshStandardMaterial({ color: 0xfefefe });
+        const floor = new THREE.Mesh(geometry, material);
+        floor.rotation.x = -Math.PI / 2;
+        floor.receiveShadow = true;
+        this.scene.add(floor);
+    }
+
+    loadSphere() {
+        const materialScale = 1.25;
+
+        const materials = {
+            mat2: {
+                base: this.texture.load("../static/models/mat1/base.jpg"),
+                height: this.texture.load("../static/models/mat1/height.jpg"),
+                ao: this.texture.load("../static/models/mat1/ao.jpg"),
+                norm: this.texture.load("../static/models/mat1/norm.jpg"),
+                rough: this.texture.load("../static/models/mat1/rough.jpg"),
+            },
+            mat1: {
+                base: this.texture.load("../static/models/mat2/base.jpg"),
+                height: this.texture.load("../static/models/mat2/height.jpg"),
+                ao: this.texture.load("../static/models/mat2/ao.jpg"),
+                norm: this.texture.load("../static/models/mat2/norm.jpg"),
+                rough: this.texture.load("../static/models/mat2/rough.jpg"),
+            },
+            mat3: {
+                base: this.texture.load("../static/models/matLion/base.jpg"),
+                height: this.texture.load("../static/models/matLion/height.jpg"),
+                ao: this.texture.load("../static/models/matLion/ao.jpg"),
+                norm: this.texture.load("../static/models/matLion/norm.jpg"),
+                rough: this.texture.load("../static/models/matLion/rough.jpg"),
+            },
+        };
+
+        let material = new THREE.MeshPhysicalMaterial({
+            map: materials.mat1.base,
+            aoMap: materials.mat1.ao,
+            aoMapIntensity: 1,
+            normalMap: materials.mat1.norm,
+            displacementMap: materials.mat1.height,
+            displacementScale: 0.03,
+            roughnessMap: materials.mat1.rough,
+            metalness: 0,
+            clearcoat: 0,
+            flatShading: false,
+            roughness: 0.9,
+            exposure: 2,
+        });
+
+        material.map.minFilter = THREE.NearestFilter;
+        material.map.generateMipmaps = false;
+        material.map.wrapS = material.map.wrapT = THREE.RepeatWrapping;
+        material.aoMap.wrapS = material.aoMap.wrapT = THREE.RepeatWrapping;
+        material.displacementMap.wrapS = material.displacementMap.wrapT = THREE.RepeatWrapping;
+        material.normalMap.wrapS = material.normalMap.wrapT = THREE.RepeatWrapping;
+        material.roughnessMap.wrapS = material.roughnessMap.wrapT = THREE.RepeatWrapping;
+
+        material.map.repeat.set(materialScale, materialScale);
+        material.aoMap.repeat.set(materialScale, materialScale);
+        material.displacementMap.repeat.set(materialScale, materialScale);
+        material.normalMap.repeat.set(materialScale, materialScale);
+        material.roughnessMap.repeat.set(materialScale, materialScale);
+        //
+        material.needsUpdate = true;
+        // material.sheen = true;
+        material.sheen = new THREE.Color(10, 10, 10);
 
         this.gui
             .add(this.guiConf, "sphereMaterial", {
@@ -326,7 +325,7 @@ export default class FurnitureConfigurator {
                 Material3: 3,
             })
             .onChange((value) => {
-                this.transformMaterial(value, material, materials, 1);
+                this.transformMaterial(value, material, materials, materialScale);
             });
 
         setTimeout(() => {
@@ -335,9 +334,11 @@ export default class FurnitureConfigurator {
     }
 
     sphere(material) {
-        const geometry = new THREE.SphereGeometry(1.5, 400, 400);
+        const geometry = new THREE.SphereGeometry(2.5, 400, 400);
         const sphere = new THREE.Mesh(geometry, material);
-        sphere.position.set(4, 2, 0);
+        sphere.position.set(0, 2.5, 0);
+        sphere.castShadow = true;
+        sphere.receiveShadow = true;
         this.scene.add(sphere);
 
         gsap.to(sphere.rotation, {
--- static/js/components/__FurnitureConfigurator.js
+++ static/js/components/__FurnitureConfigurator.js
@@ -65,7 +65,7 @@ export default class FurnitureConfigurator {
         this.controls.enableDamping = true;
         this.controls.minDistance = 1;
         this.controls.maxDistance = 10;
-        this.controls.target.set(0, 0.35, 0);
+        this.controls.target.set(0, 0.5, 0);
         this.controls.update();
 
         window.addEventListener("resize", this.onWindowResize);
@@ -92,3 +92,186 @@ export default class FurnitureConfigurator {
         this.renderer.render(this.scene, this.camera);
     }
 }
+
+// <script type="module">
+//
+//     import * as THREE from '../build/three.module.js';
+//
+//     import * as Nodes from './jsm/nodes/Nodes.js';
+//
+//     import Stats from './jsm/libs/stats.module.js';
+//     import { GUI } from './jsm/libs/dat.gui.module.js';
+//
+//     import { OrbitControls } from './jsm/controls/OrbitControls.js';
+//
+//     import { FBXLoader } from './jsm/loaders/FBXLoader.js';
+//
+//     // Graphics variables
+//     var camera, controls, scene, renderer, stats;
+//     var directionalLight;
+//     var mesh, sphere, material, nodeMaterial;
+//
+//     var params = {
+//         nodeMaterial: true,
+//         color: new THREE.Color( 255, 0, 127 ),
+//         sheenBRDF: true,
+//         sheen: new THREE.Color( 10, 10, 10 ), // corresponds to .04 reflectance
+//         roughness: .9,
+//         exposure: 2,
+//     };
+//
+//     // model
+//     new FBXLoader().load( 'models/fbx/cloth.fbx', function ( loadedModel ) {
+//
+//     mesh = loadedModel.children[0];
+//
+//     init();
+//
+//     } );
+//
+//     function init( ) {
+//
+//     camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.2, 2000 );
+//
+//     scene = new THREE.Scene();
+//     scene.background = new THREE.Color( 0xbfd1e5 );
+//
+//     mesh.scale.multiplyScalar( .5 );
+//     scene.add( mesh );
+//
+//     //
+//
+//     material = new THREE.MeshPhysicalMaterial();
+//     material.side = THREE.DoubleSide;
+//     material.metalness = 0;
+//
+//     //
+//
+//     nodeMaterial = new Nodes.StandardNodeMaterial();
+//     nodeMaterial.side = THREE.DoubleSide;
+//     nodeMaterial.metalness = new Nodes.FloatNode( 0 );
+//     nodeMaterial.roughness = new Nodes.FloatNode();
+//     nodeMaterial.color = new Nodes.ColorNode( params.color.clone() );
+//
+//     //
+//
+//     sphere = new THREE.Mesh(
+//     new THREE.SphereBufferGeometry( 1, 100, 100 ),
+//     material
+//     );
+//     scene.add(sphere);
+//
+//     camera.position.set( - 12, 7, 4 );
+//
+//     var container = document.getElementById( 'container' );
+//     renderer = new THREE.WebGLRenderer();
+//     renderer.setPixelRatio( window.devicePixelRatio );
+//     renderer.setSize( window.innerWidth, window.innerHeight );
+//     renderer.shadowMap.enabled = true;
+//     container.appendChild( renderer.domElement );
+//
+//     controls = new OrbitControls( camera, renderer.domElement );
+//     controls.target.set( 0, 2, 0 );
+//     controls.update();
+//
+//     directionalLight = new THREE.DirectionalLight( 0xffffff, .5 );
+//     directionalLight.position.set( 0, 10, 0 );
+//     directionalLight.castShadow = true;
+//     directionalLight.add(
+//     new THREE.Mesh(
+//     new THREE.SphereBufferGeometry( .5 ),
+//     new THREE.MeshBasicMaterial( { color: 0xffffff } )
+//     )
+//     );
+//
+//     scene.add( directionalLight );
+//
+//     stats = new Stats();
+//     stats.domElement.style.position = 'absolute';
+//     stats.domElement.style.top = '0px';
+//     container.appendChild( stats.dom );
+//
+//     window.addEventListener( 'resize', onWindowResize, false );
+//
+//     var gui = new GUI();
+//
+//     function onUpdate() {
+//
+//     mesh.material = sphere.material = params.nodeMaterial
+//     ? nodeMaterial
+//     : material;
+//
+//     material.sheen = params.sheenBRDF
+//     ? new THREE.Color()
+//     : null;
+//
+//     material.needsUpdate = true;
+//
+//     nodeMaterial.sheen = params.sheenBRDF
+//     ? new Nodes.ColorNode( material.sheen )
+//     : undefined;
+//
+//     nodeMaterial.needsCompile = true;
+//
+// }
+//
+//     gui.add( params, 'nodeMaterial' ).onChange( onUpdate );
+//     gui.addColor( params, 'color' );
+//     gui.add( params, 'sheenBRDF' ).onChange( onUpdate );
+//     gui.addColor( params, 'sheen' );
+//     gui.add( params, 'roughness', 0, 1 );
+//     gui.add( params, 'exposure', 0, 3 );
+//     gui.open();
+//
+//     onUpdate();
+//
+//     animate();
+//
+// }
+//
+//     function onWindowResize() {
+//
+//     camera.aspect = window.innerWidth / window.innerHeight;
+//     camera.updateProjectionMatrix();
+//
+//     renderer.setSize( window.innerWidth, window.innerHeight );
+//
+// }
+//
+//     function animate() {
+//
+//     requestAnimationFrame( animate );
+//
+//     render();
+//     stats.update();
+//
+// }
+//
+//     function render() {
+//
+//     //
+//
+//     material.color.copy( params.color ).multiplyScalar( 1 / 255 );
+//     material.roughness = params.roughness;
+//
+//     //
+//
+//     nodeMaterial.color.value.copy( material.color );
+//     nodeMaterial.roughness.value = params.roughness;
+//
+//     //
+//
+//     if ( params.sheenBRDF ) {
+//
+//     material.sheen.copy( params.sheen ).multiplyScalar( 1 / 255 );
+//
+// }
+//
+//     //
+//
+//     renderer.toneMappingExposure = params.exposure;
+//     renderer.render( scene, camera );
+//
+// }
+//
+// </script>
